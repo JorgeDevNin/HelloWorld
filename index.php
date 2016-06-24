67050314c88694d9d9ff6f9766ec2a018268827a
JorgeDevNin <jor_2292@hotmail.com>
2016-06-24 19:41:34 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+Codigo generado usando Netbeans, Desarrollador: Jorge
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Test Git PHP</title>
+    </head>
+    <body>
+        <?php
+           echo "Hola";
+        ?>
+    </body>
+</html>
